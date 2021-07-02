724f1163fa0e9e0ae28b495f0dab0e3c33ae3101
LucasDev13 <lucaspontes140@gmail.com>
2021-07-02 01:20:04 -0300

    Implementado a lógica para validar o nome da categoria. Criado a classe ValidatorNameCategory e criado a lógica de validação. Criado o método findByNameCategory no repository. Adicionado no CategoryController o método init para adicionar o validador.

--- src/main/java/br/com/casadocodigo/controller/CategoryController.java
+++ src/main/java/br/com/casadocodigo/controller/CategoryController.java
@@ -1,14 +1,14 @@
 package br.com.casadocodigo.controller;
 
+import br.com.casadocodigo.config.validation.ValidatorNameCategory;
 import br.com.casadocodigo.repository.CategoryRepository;
 import br.com.casadocodigo.request.CategoryRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
@@ -18,7 +18,16 @@ public class CategoryController {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ValidatorNameCategory validatorNameCategory;
+
+    @InitBinder
+    public void init(WebDataBinder binder){
+        binder.addValidators(validatorNameCategory);
+    }
+
     @PostMapping
+    @Transactional
     public ResponseEntity<?> saveCategory(@RequestBody @Valid CategoryRequest categoryRequest){
         categoryRepository.save(categoryRequest.toModel());
         return ResponseEntity.ok().build();
--- src/main/java/br/com/casadocodigo/repository/CategoryRepository.java
+++ src/main/java/br/com/casadocodigo/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package br.com.casadocodigo.repository;
 import br.com.casadocodigo.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByNameCategory(String nameCategory);
 }
